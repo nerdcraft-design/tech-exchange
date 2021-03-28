d0daa9cdad9e2ce6b44017ff010adaf1b3101f3b
Ian Jackson <ijacksondesign@gmail.com>
2021-03-28 17:53:28 -0400

    finished styles and added idle logout

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -40,7 +40,7 @@ async function signupFormHandler(event) {
         })
         // check the response status
         if (response.ok) {
-            console.log('success');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
--- server.js
+++ server.js
@@ -9,15 +9,18 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const randomstring = require("randomstring");
-
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: randomstring.generate(),
-    cookie: {},
+    cookie: {
+      maxAge: 1000 * 60
+    },
     resave: false,
-    saveUninitialized: true,
+    rolling: true,
+    saveUninitialized: false,
     store: new SequelizeStore({
       db: sequelize
     })
@@ -31,7 +34,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/'));
+app.use(routes);
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
